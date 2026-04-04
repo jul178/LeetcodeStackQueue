e833cee4f57b0e896dc9fc58442af9a65e95c8b0
Julia Dub <juliadub2008@gmail.com>
2026-04-04 18:21:38 +0300

    Initial commit with MyQueue and Stack

---
+++ implement_queue_using_stacks/solution.py
@@ -0,0 +1,19 @@
+class MyQueue:
+    def __init__(self):
+        self.stack_in = []
+        self.stack_out = []
+
+    def push(self, x: int) -> None:
+        self.stack_in.append(x)
+
+    def pop(self) -> int:
+        return self.stack_out.pop()
+
+    def peek(self) -> int:
+        if not self.stack_out:
+            while self.stack_in:
+                self.stack_out.append(self.stack_in.pop())
+        return self.stack_out[-1]
+
+    def empty(self) -> bool:
+        return self.stack_in == [] and self.stack_out == []
