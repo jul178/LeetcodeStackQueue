6ddaa95f6aca1e606c6095e6948874aaf19225ea
Julia Dub <juliadub2008@gmail.com>
2026-04-04 20:00:27 +0300

    Initial commit: implemented Queue using two Stacks

--- implement_queue_using_stacks/solution.py
+++ implement_queue_using_stacks/solution.py
@@ -1,19 +1,51 @@
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, item):
+        self.head = Node(item, next=self.head)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        val = self.head.val
+        self.head = self.head.next
+        return val
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.head.val
+
+    def is_empty(self):
+        return self.head is None
+
+
 class MyQueue:
     def __init__(self):
-        self.stack_in = []
-        self.stack_out = []
+        self.stack_in = Stack()
+        self.stack_out = Stack()
 
     def push(self, x: int) -> None:
-        self.stack_in.append(x)
+        self.stack_in.push(x)
 
     def pop(self) -> int:
+        if self.stack_out.is_empty():
+            while not self.stack_in.is_empty():
+                self.stack_out.push(self.stack_in.pop())
         return self.stack_out.pop()
 
     def peek(self) -> int:
-        if not self.stack_out:
-            while self.stack_in:
-                self.stack_out.append(self.stack_in.pop())
-        return self.stack_out[-1]
+        if self.stack_out.is_empty():
+            while not self.stack_in.is_empty():
+                self.stack_out.push(self.stack_in.pop())
+        return self.stack_out.peek()
 
     def empty(self) -> bool:
-        return self.stack_in == [] and self.stack_out == []
+        return self.stack_in.is_empty() and self.stack_out.is_empty()
